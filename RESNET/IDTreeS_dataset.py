3f7b924f6a45e04eb9c9a9f75a3d1aec01ddbb07
Francesco Nattino <f.nattino@esciencecenter.nl>
2023-08-31 17:19:10 +0200

    add first class balance

--- RESNET/IDTreeS_dataset.py
+++ RESNET/IDTreeS_dataset.py
@@ -7,24 +7,25 @@ import rioxarray
 import numpy as np
 
 from rioxarray.exceptions import NoDataInBounds
-from sklearn.preprocessing import LabelEncoder 
+from sklearn.preprocessing import LabelEncoder
 
 
 MIN_PIXEL_SIZE_DEF = 32
 MAX_PIXEL_SIZE_DEF = 100
 PIXEL_SIZE_DEF = 100
+MIN_SAMPLE_SIZE_DEF = 10
 
 
 class IDTreeSDataset:
-    
     def __init__(
-            self, 
-            rgb_paths, 
-            bboxes_paths, 
+            self,
+            rgb_paths,
+            bboxes_paths,
             classes_path,
             min_pixel_size=None,
             max_pixel_size=None,
             pixel_size=None,
+            min_sample_size=None,
         ):
         self.rgb_paths = rgb_paths
         self.bboxes_paths = bboxes_paths
@@ -36,31 +37,29 @@ class IDTreeSDataset:
             else MAX_PIXEL_SIZE_DEF
         self.pixel_size = pixel_size if pixel_size is not None \
             else PIXEL_SIZE_DEF
+        self.min_sample_size = min_sample_size if min_sample_size is not None \
+            else MIN_SAMPLE_SIZE_DEF
 
         self.classes = None
         self.bboxes = None
 
     def _load_classes(self):
-        """ Load classes and convert them to categorical values. """
+        """ Load data classes. """
         classes = pd.read_csv(self.classes_path)
-        
-        # drop duplicate entries - repeated labels appear to be the same
+
+        # drop duplicate entries - at least repeated entries
+        # seem to have the same class label
         classes = classes.drop_duplicates(subset='indvdID')
 
-        # convert classes to categorical values
-        le = LabelEncoder()
-        le.fit(classes.taxonID)
-        classes['categorical_label'] = le.transform(classes.taxonID)
-
         classes = classes.set_index('indvdID')
-        self.classes = classes['categorical_label']
+        self.classes = classes['taxonID']
 
     def _load_bboxes(self):
-        """ 
+        """
         Load bounding boxes, retaining the only tree IDs for which
-        a class is available (classes need to be loaded first). 
+        a class is available (classes need to be loaded first).
         Multiple boxes can exist for a given tree ID (presumably
-        trees at the boundary between images). 
+        trees crossing the edges between images).
         """
         bboxes = pd.concat([
             geopandas.read_file(p)
@@ -68,11 +67,17 @@ class IDTreeSDataset:
         ])
 
         bboxes = bboxes.set_index('indvdID')
-        # if GeoDataFrame is left, the data structure is maintained
-        bboxes = pd.merge(bboxes, self.classes, how='right', on='indvdID')
-        self.bboxes = bboxes['geometry'] 
 
-    def _generate_cutouts_for_image(self, rgb_path): 
+        # use GeoDataFrame as left dataframe (preserves object type)
+        bboxes = pd.merge(
+            bboxes,
+            self.classes,
+            how='right',
+            on='indvdID'
+        )
+        self.bboxes = bboxes['geometry']
+
+    def _generate_cutouts_for_image(self, rgb_path):
         """ Extract all cutouts for a given image. """
 
         if self.classes is None:
@@ -88,10 +93,10 @@ class IDTreeSDataset:
         xmin, ymin, xmax, ymax = rgb.rio.bounds()
         bboxes_clip = self.bboxes.cx[xmin:xmax, ymin:ymax]
         bboxes_clip = bboxes_clip[~bboxes_clip.is_empty]
- 
+
         for indvdID, bbox in bboxes_clip.items():
 
-            # some polygons have very small intersections with the images
+            # some polygons have very small intersections
             try:
                 cutout = rgb.rio.clip([bbox], drop=True)
             except NoDataInBounds:
@@ -99,7 +104,7 @@ class IDTreeSDataset:
 
             # drop large or small cutouts
             if (
-                _large_image(cutout.data, self.max_pixel_size) 
+                _large_image(cutout.data, self.max_pixel_size)
                 or _small_image(cutout.data, self.min_pixel_size)
             ):
                 continue
@@ -109,20 +114,8 @@ class IDTreeSDataset:
 
             yield indvdID, label, img
 
-    def generate_cutouts(self):
-        """ 
-        Return cutouts extracted from all images (as a generator).
-        """
-        for rgb_path in self.rgb_paths:
-            cutouts = self._generate_cutouts_for_image(rgb_path)
-            for tree_id, label, img in cutouts:
-                yield tree_id, label, img
-    
-    def get_all_cutouts(self):
-        """ 
-        Load all cutouts from all images and return them as a 
-        Numpy array. 
-        """
+    def get_cutouts(self):
+        """ Load cutouts from all images. """
         tree_ids = []
         labels = []
         imgs = []
@@ -132,11 +125,36 @@ class IDTreeSDataset:
                 tree_ids.append(tree_id)
                 labels.append(label)
                 imgs.append(img)
-        return (
-            np.asarray(tree_ids), 
+
+        tree_ids, labels, imgs = _balance_classes(
+            np.asarray(tree_ids),
             np.asarray(labels),
             np.stack(imgs),
+            self.min_sample_size
         )
+
+        # convert class labels to categorical values
+        le = LabelEncoder()
+        le.fit(labels)
+        labels_categorical = le.transform(labels)
+        return tree_ids, labels_categorical, imgs
+
+
+def _balance_classes(tree_ids, labels, imgs, min_sample_size):
+    """
+    Balance class compositions by dropping classes with
+    few samples and by performing data augmentation.
+    """
+    labels_unique, counts = np.unique(labels, return_counts=True)
+    labels_to_keep = labels_unique[counts >= min_sample_size]
+    mask = np.isin(labels, labels_to_keep)
+
+    # data augmentation here?
+    return (
+        tree_ids[mask],
+        labels[mask],
+        imgs[mask],
+    )
 
 
 def _large_image(data, max_pixel_size):
@@ -154,33 +172,27 @@ def _small_image(data, min_pixel_size):
 def _pad_image(data, pixel_size):
     """ Pad each image """
     pad_width_x1 = np.floor((pixel_size - data.shape[1])/2).astype(int)
-    pad_width_x2 = pixel_size - data.shape[1] - pad_width_x1 
+    pad_width_x2 = pixel_size - data.shape[1] - pad_width_x1
     pad_width_y1 = np.floor((pixel_size - data.shape[2])/2).astype(int)
     pad_width_y2 = pixel_size - data.shape[2] - pad_width_y1
     data = np.pad(data, pad_width=[(0, 0),(pad_width_x1, pad_width_x2),(pad_width_y1, pad_width_y2)], mode='constant')
     return data
-        
+
 
 if __name__ == "__main__":
     rgb_paths = glob.glob('./train/RemoteSensing/RGB/*.tif')
     bboxes_paths = glob.glob('./train/ITC/train_*.shp')
     classes_path = './train/Field/train_data.csv'
 
-    # use as a generator ...
-    print("Run as a generator ...")
-    ds = IDTreeSDataset(rgb_paths, bboxes_paths, classes_path)
-    cutouts = ds.generate_cutouts()
-
-    s = time.time()
-    for cutout in ds.generate_cutouts():
-        _, _, _ = cutout
-    print(f"Executed in {time.time() - s}")
-
-    # ... or load all data at once
-    print("Load all cutouts ...")
+    print("Loading all cutouts ...")
     ds = IDTreeSDataset(rgb_paths, bboxes_paths, classes_path)
 
     s = time.time()
-    cutouts = ds.get_all_cutouts()
+    ids, labels, cutouts = ds.get_cutouts()
     print(f"Executed in {time.time() - s}")
-    print(cutouts[-1].shape)
+
+    print(f"Num. of samples: {len(ids)}")
+    ll, cc = np.unique(labels, return_counts=True)
+    print(f"Num. classes: {len(ll)}")
+    print(f"Min. population, Max. population: {min(cc), max(cc)}")
+    print(f"Image shape: {cutouts.shape[1:]}")
